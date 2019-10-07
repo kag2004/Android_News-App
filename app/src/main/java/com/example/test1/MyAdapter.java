bd01c429bef23a979ea58078edafcd6e1f55806a
kag2004 <kag2004@gmail.com>
2019-10-07 21:15:40 +0900

    뉴스 앱 종료 추가 내용 추 후 계속 업데이트

---
+++ app/src/main/java/com/example/test1/MyAdapter.java
@@ -0,0 +1,92 @@
+package com.example.test1;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private List<NewsData> mDataset;
+    private static View.OnClickListener onClickListener;
+    //RecyclerView.ViewHolder 이걸 사용 하는 이유는 메모르 관리 효율적으로 관리 해준다는 장점 때문에
+    //xml파일이 하나로 계속 View를 만들필요 없이 여기에서 자동적으로 하나하나 필요 할 때마다 써줄 수 있다
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        //onCreateViewHolder 한줄에 들어가는 요소를 선택하는 거
+        //전체 받아온 것을 하나 하나 풀어 주는 쪽 이라 생각하면 됨
+        // each data item is just a string in this case
+        public TextView TextView_title;
+        public TextView TextView_description;
+        public SimpleDraweeView ImageView_title;
+//        public SimpleDraweeView ImageView_title;
+        //4.여기서 자식들 뷰들이 찾아서 쓴다.
+        public MyViewHolder(View v) {
+            super(v);
+            TextView_title = v.findViewById(R.id.TextView_title);
+            TextView_description = v.findViewById(R.id.TextView_description);
+            ImageView_title = (SimpleDraweeView) v.findViewById(R.id.ImageView_title);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    //어뎀터 최초 셋팅 해준다 초기 데이터 들이 여기로 들어와 셋팅~~>>
+    //어뎁터 받아온 받아온걸 위에 자식에게 뿌려준다.
+    //+ 액티비티가 아닌 곳에서 Context를 쓰기 위해 액티비티에서 Context값을 넘겨 가져 온다.(해보면서 이해 해야할듯)
+    //하지만 액티비티 Context를 하면 누수가 생겨서 좋은 점은 아님
+    public MyAdapter(List<NewsData> myDataset, Context context) {
+        mDataset = myDataset;
+        Fresco.initialize(context);
+
+    }
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        //하나의 로우를 이미지 파일을 연결 하는 곳  onCreateViewHolder 이부분 영역
+        //1.애는 뷰 홀더 RecyclerView의 항목 화면 연결은 onCreateViewHolder 함수
+        //2.즉 전체 를 받아오는 부분 이고
+        // create a new view
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_news, parent, false);
+        //3.row_news 가 부모 이다.
+        //3.이걸 MyViewHolder 이 녀석을 이용해 v를 public MyViewHolder(View v) 에서 하나하나 찾아 가서
+        //
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    //5.반복 될때 마다 데이터를 셋팅 하는 곳 onBindViewHolder
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+    //해당 뉴스 정보를 가져 오게 포지션 즉 리사이클러뷰 [] 배열의 00번째 - 데이터 00번째 가져와라 이런 느낌
+        //List에 순서대로 add하여 넣었으니 get으로 해당 순버 뉴스 데이터를 꺼내 온다. (저런 느낌)
+//        holder.TextView_title.setText(mDataset.get(position).getTitle());
+        NewsData news = mDataset.get(position);
+
+        holder.TextView_title.setText(news.getTitle());
+        holder.TextView_description.setText(news.getDescription());
+        //이미지를 가져 오는 방법은 약간 다름
+        //1. fresco를 사용 Fresco는 이미지를 표시하기 위한 레아이웃 SimpleDraweeView를 사용 한다.
+        //2. 이런 방식은 해당 Uri 주소에 있는 이미지를 가져 오는 방식이다.
+        Uri uri = Uri.parse(news.getUrlToImage());//데이터에 들어있는 이미지 주소 바꿉
+        holder.ImageView_title.setImageURI(uri);  //해당 Uri 주소 이미지 가져 오기 위한
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        //삼항 연산 mDataset == null조건이 트루면 0 / 거짓 mDataset.size 조건식
+        return mDataset == null ? 0 : mDataset.size();
+    }
+
+}
---
+++ app/src/main/java/com/example/test1/NewsActivity.java
@@ -0,0 +1,113 @@
+package com.example.test1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] mDataset ={"1","2"};
+
+    //Volley : newRequestQueue == Request 요청이 들어오면 들어노는 순서대로 뿌려주는 역활
+    RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+        //activity_news 에 연결
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        //하나의 로우 사이즈 맞춤
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        //개가 어디 방향인지 LinearLayoutManager 해줌
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        queue = Volley.newRequestQueue(this);
+        getNews();//여기까지가 1번 과정
+        //1. 화면 로딩 -> 뉴스 정보를 받아온다.
+
+        //2. 정보-> 어댑터 넘겨준다.
+        //3. 어댑터 -> 셋팅
+    }
+    public void getNews(){
+
+        String url ="https://newsapi.org/v2/top-headlines?country=kr&apiKey=3d5dcab149914c7bb8339c92f62ffb5d";
+
+        // Request a string response from the provided URL.
+        // 요청을 하면 어떠한 문자열 반응이 온다.
+        //
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override//뉴스 API에 있는 정보가 쭉 들어온다.
+                    // response 데이터를 가져와서 List<NewsData>배열있는걸 news.add(newsData); 넣어 ->>
+                    //그걸 mAdapter = new MyAdapter(news); 어뎁터에 넘기는 작업
+                    public void onResponse(String response) {
+                        //Log.d(" NEWS", response);
+
+                //public void onResponse(String response) 으로 가져오면 문자열인데 그걸 제이슨 오브젝트 형식으로 바꿔서 가져 오려고 하려고
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+
+                            JSONArray arrayArticles = jsonObj.getJSONArray("articles");
+
+                            //response ->>NewsData Class 분류
+                            List<NewsData> news = new ArrayList<>(); //많은 데이터가 담겨 있어서 배열로 사용
+                            for (int i = 0 , j = arrayArticles.length(); i < j; i++){
+                                JSONObject obj = arrayArticles.getJSONObject(i);
+
+                                Log.d("NEWS", obj.toString());
+
+                                NewsData newsData = new NewsData(); //클래스 선언 쓰겠다고
+                                newsData.setTitle(obj.getString("title"));
+                                newsData.setUrlToImage(obj.getString("urlToImage"));
+                                newsData.setDescription(obj.getString("description"));
+                                news.add(newsData);
+                            }
+
+                            //여기가 mAdapter, mRecyclerView 뉴스 모든 정보가 여기로 들어온다.
+                            //NewsData 클래스에서 NewsData(즉 Mydata)
+                            mAdapter = new MyAdapter(news, NewsActivity.this);
+                            mRecyclerView.setAdapter(mAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        // stringRequest 에있는 정보를 ==>queue 에 담아준다.
+        queue.add(stringRequest);
+    }
+
+}
